72fb3f585584f9075537ffdb1224b3e0c215ccc1
Muriel Damazio <murieldps@gmail.com>
2022-03-07 21:24:00 -0300

    chore: Adding mongoose and dotenv. Connecting to database

--- src/server.js
+++ src/server.js
@@ -1,6 +1,12 @@
-import express, { response } from "express"
+const dotenv = require("dotenv").config()
+const express = require("express")
+
+const connectToDatabase = require("./database")
+
+connectToDatabase()
 
 const app = express()
 
+app.use(express.json());
 
 app.listen(3333, () => console.log("Server is running at 3333"));
